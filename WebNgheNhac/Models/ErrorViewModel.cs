3f1df0b9d6437bdf9bfca83b1608ceab10391513
jeremytran4496-prog <jeremytran4496@gmail.com>
2025-09-21 09:55:41 +0700

    Delete WebNgheNhac directory

--- WebNgheNhac/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebNgheNhac.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
